7eb8197ded6fb936199426897afbbf204a8de73a
shubham sharma <shubhamsharma4539@gmail.com>
2024-04-28 14:18:35 +0530

    api for country listing

--- src/main/java/com/university/mcmaster/controllers/MainController.java
+++ src/main/java/com/university/mcmaster/controllers/MainController.java
@@ -1,21 +1,42 @@
 package com.university.mcmaster.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.university.mcmaster.models.dtos.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
 
     @GetMapping("/version")
-    public ResponseEntity<?> version(){
+    public ResponseEntity<?> version() {
         return ResponseEntity.ok("v0.0.1");
     }
 
     @GetMapping("/health")
-    public ResponseEntity<?> health(){
+    public ResponseEntity<?> health() {
         return ResponseEntity.ok("ok");
+    }
+
+    @GetMapping("/countries")
+    public ResponseEntity<?> getCountries() {
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap<String, Object> jsonMap = null;
+        try {
+            jsonMap = mapper.readValue(new File("src/main/resources/static/countries.json"), HashMap.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(ApiResponse.builder()
+                .data(jsonMap)
+                .build());
     }
 }
