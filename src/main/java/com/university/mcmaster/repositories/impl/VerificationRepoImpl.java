51c91cf13f116c207f1d6e29f5337b47f7b4a96f
shubham sharma <shubhamsharma4539@gmail.com>
2024-05-18 15:58:08 +0530

    config bug fix

--- src/main/java/com/university/mcmaster/repositories/impl/VerificationRepoImpl.java
+++ src/main/java/com/university/mcmaster/repositories/impl/VerificationRepoImpl.java
@@ -5,9 +5,11 @@ import com.university.mcmaster.models.entities.SheerIdVerificationData;
 import com.university.mcmaster.repositories.VerificationRepo;
 import com.university.mcmaster.repositories.utils.FirebaseUtils;
 import com.university.mcmaster.utils.FirestoreConstants;
+import org.springframework.stereotype.Repository;
 
 import javax.management.Query;
 
+@Repository
 public class VerificationRepoImpl extends FirebaseUtils<SheerIdVerificationData> implements VerificationRepo {
     @Override
     public SheerIdVerificationData getLatestSheerIdVerificationByEmailAndStatusSuccess(String email) {
