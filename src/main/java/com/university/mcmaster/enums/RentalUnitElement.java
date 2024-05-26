d1c7d9cc2198be129281b019b0015ec87d69c914
shubham sharma <shubhamsharma4539@gmail.com>
2024-05-26 19:37:21 +0530

    new rental unit elements

--- src/main/java/com/university/mcmaster/enums/RentalUnitElement.java
+++ src/main/java/com/university/mcmaster/enums/RentalUnitElement.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 public enum RentalUnitElement {
     living_room(2)
     ,bed_room(2)
+    ,beds(2)
     ,dining_room(2)
     ,bath_room(2)
+    ,baths(2)
     ,kitchen(2)
     ,others(5);
 
@@ -20,6 +22,8 @@ public enum RentalUnitElement {
     public static int getTotalAllowedFiles() {
         return living_room.getAllowedFiles() +
         bed_room.getAllowedFiles() +
+        baths.getAllowedFiles() +
+        beds.getAllowedFiles() +
         dining_room.getAllowedFiles() +
         bath_room.getAllowedFiles() +
         kitchen.getAllowedFiles() +
