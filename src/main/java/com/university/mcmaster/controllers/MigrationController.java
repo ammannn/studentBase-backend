b8892502be9e8245b8e2074e27d72732b2ae4b72
shubham sharma <shubhamsharma4539@gmail.com>
2024-05-29 17:16:35 +0530

    index api

--- src/main/java/com/university/mcmaster/controllers/MigrationController.java
+++ src/main/java/com/university/mcmaster/controllers/MigrationController.java
@@ -4,6 +4,7 @@ import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.storage.*;
 import com.google.common.collect.ImmutableList;
 import com.google.firebase.cloud.FirestoreClient;
+import com.stripe.model.tax.Registration;
 import com.university.mcmaster.enums.ApplicationStatus;
 import com.university.mcmaster.enums.FilePurpose;
 import com.university.mcmaster.enums.UserRole;
@@ -12,7 +13,7 @@ import com.university.mcmaster.models.entities.Application;
 import com.university.mcmaster.models.entities.Dashboard;
 import com.university.mcmaster.models.entities.StudentDocFile;
 import com.university.mcmaster.models.entities.User;
-import com.university.mcmaster.repositories.UserRepo;
+import com.university.mcmaster.repositories.*;
 import com.university.mcmaster.utils.EnvironmentVariables;
 import com.university.mcmaster.utils.FirestoreConstants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,20 @@ public class MigrationController {
 
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private AdminRepo adminRepo;
+    @Autowired
+    private LikeAndRatingRepo likeAndRatingRepo;
+    @Autowired
+    private RentalUnitRepo rentalUnitRepo;
+    @Autowired
+    private ApplicationRepo applicationRepo;
+    @Autowired
+    private PaymentRepo paymentRepo;
+    @Autowired
+    private CalendarRepo calendarRepo;
+    @Autowired
+    private FileRepo fileRepo;
 
     @GetMapping("/user-doc")
     public ResponseEntity<?> fixUserDocsMap() {
@@ -112,5 +127,50 @@ public class MigrationController {
             e.printStackTrace();
         }
         return ResponseEntity.ok("updated field");
+    }
+
+    @GetMapping("/index-check")
+    public ResponseEntity<?> indexApi(){
+        userRepo.findUserByEmail("test");
+        userRepo.getAllUsersByRole(UserRole.admin);
+        userRepo.getPaginatedUsersByVerificationStatusForAdmin(VerificationStatus.failed,10,"test");
+        userRepo.getPaginatedUsersByVerificationStatusForAdmin(VerificationStatus.failed,0,null);
+        userRepo.getPaginatedUsersByVerificationStatusForAdmin(VerificationStatus.failed,10,null);
+        userRepo.getPaginatedUsersByVerificationStatusForAdmin(VerificationStatus.failed,0,"test");
+        likeAndRatingRepo.getLikeAndRatingDocsByUserIdAndDeletedFalse("test");
+        likeAndRatingRepo.findByUserIdAndRentalUnitIdAndDeletedFalse("test","test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test","test",100l,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(null,"test","test","test",100l,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test",null,"test",100l,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test",null,100l,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test","test",0,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test","test",100l,0,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test","test",100l,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test","test",100l,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test",null,null,100l,100l,"test",100,null);
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test","test",100l,100l,"test",100,null);
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test","test","test",100l,100l,"test",100,"test");
+        rentalUnitRepo.getPaginatedRentalUnitsByEligibilityTrueAndDeletedFalseAndSearchFilters(List.of("test"),"test",null,null,0,0,"test",100,"test");
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalse("test",10,"test");
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalse("test",0,null);
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalse("test",0,"test");
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalse("test",10,null);
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalseAndEligibilityForListing("test",true,10,null);
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalseAndEligibilityForListing("test",false,10,null);
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalseAndEligibilityForListing("test",false,10,"null");
+        rentalUnitRepo.getRentalUnitByUserIdAndDeletedFalseAndEligibilityForListing("test",false,0,"null");
+        applicationRepo.getApplicationByStudentIdAndRentalUnitIdAndDeletedFalse("test","test");
+        applicationRepo.getApplicationByNullableRentalUnitIdAndStatusAndDeletedFalse("test",ApplicationStatus.approved);
+        applicationRepo.getApplicationByNullableRentalUnitIdAndStatusAndDeletedFalse(null,ApplicationStatus.approved);
+        applicationRepo.getPaginatedApplicationsByStudentIdAndNullableRentalUnitIdAndStatusAndDeletedFalse("null","test",ApplicationStatus.approved,10,"test");
+        applicationRepo.getPaginatedApplicationsByStudentIdAndNullableRentalUnitIdAndStatusAndDeletedFalse("null","test",ApplicationStatus.approved,10,null);
+        applicationRepo.getPaginatedApplicationsByStudentIdAndNullableRentalUnitIdAndStatusAndDeletedFalse("null","test",ApplicationStatus.approved,0,null);
+        applicationRepo.getPaginatedApplicationsByStudentIdAndNullableRentalUnitIdAndStatusAndDeletedFalse("null","test",null,0,null);
+        applicationRepo.getPaginatedApplicationsByStudentIdAndNullableRentalUnitIdAndStatusAndDeletedFalse("null",null,ApplicationStatus.approved,0,null);
+        applicationRepo.getPaginatedApplicationsByStudentIdAndNullableRentalUnitIdAndStatusAndDeletedFalse("null",null,null,0,null);
+        fileRepo.getFilesByRentalUnitIdAndDeletedFalseAndUploadedOnGcpTrue("test");
+        fileRepo.getFilesByRentalUnitIdAndUploadedOnGcpTrueAndDeletedFalse("test");
+        fileRepo.getFilesByUserIdAndPurposeLeaseDocAndDeletedFalse("test");
+        return ResponseEntity.ok("");
     }
 }
