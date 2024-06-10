b69bd330f4f59ec7cf3a0cc03fe46a7b5ce13850
shubham sharma <shubhamsharma4539@gmail.com>
2024-06-10 10:12:57 +0530

    version update

--- src/main/java/com/university/mcmaster/controllers/MainController.java
+++ src/main/java/com/university/mcmaster/controllers/MainController.java
@@ -34,7 +34,7 @@ public class MainController {
 
     @GetMapping("/version")
     public ResponseEntity<?> version() {
-        return ResponseEntity.ok("v0.0.2");
+        return ResponseEntity.ok("v0.0.3");
     }
 
     @GetMapping("/health")
